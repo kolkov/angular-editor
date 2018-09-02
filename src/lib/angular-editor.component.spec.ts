2ef6d85f63a9616dafaca55f5a0fa2a09ef702f3
Andy <a.kolkov@gmail.com>
2018-09-02 22:36:54 +0300

    Beginning of writing unit tests.

--- src/lib/angular-editor.component.spec.ts
+++ src/lib/angular-editor.component.spec.ts
@@ -1,6 +1,9 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AngularEditorComponent} from './angular-editor.component';
+import {AngularEditorToolbarComponent} from "./angular-editor-toolbar.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 describe('AngularEditorComponent', () => {
   let component: AngularEditorComponent;
@@ -8,7 +11,8 @@ describe('AngularEditorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AngularEditorComponent]
+      imports: [ FormsModule, HttpClientModule ],
+      declarations: [AngularEditorComponent, AngularEditorToolbarComponent, ]
     })
       .compileComponents();
   }));
--- src/lib/angular-editor.service.spec.ts
+++ src/lib/angular-editor.service.spec.ts
@@ -1,10 +1,12 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {AngularEditorService} from './angular-editor.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('AngularEditorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
       providers: [AngularEditorService]
     });
   });
