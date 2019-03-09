f70b6b9a2cec1a88cf3a8ddebeec832691198d67
Mirela Damyanova <mdamyanova181@gmail.com>
2019-03-09 14:32:46 +0200

    Add more Fonts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -47,6 +47,8 @@ export const angularEditorConfig: AngularEditorConfig = {
   fonts: [
     {class: 'arial', name: 'Arial'},
     {class: 'times-new-roman', name: 'Times New Roman'},
+    {class: 'calibri', name: 'Calibri'},
+    {class: 'comic-sans-ms', name: 'Comic Sans MS'}
   ],
   uploadUrl: 'v1/image',
 };
