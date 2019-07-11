ef7d390fcf60585b76929c27f8ade2d8753df340
Andy <a.kolkov@gmail.com>
2019-07-11 19:46:39 +0300

    Fix coverage error

--- projects/angular-editor/karma.conf.js
+++ projects/angular-editor/karma.conf.js
@@ -16,8 +16,8 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/kolkov/angular-editor'),
-      reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, '../../coverage/angular-editor'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
