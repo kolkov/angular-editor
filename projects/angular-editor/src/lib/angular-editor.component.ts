b5538b1cce3f6aac291a407f634902e1cee37705
alfredo salzillo <alfredo.salzillo@progettopa.it>
2019-08-06 10:09:20 +0200

    fix(angular-editor): fixed empty <br> when delete all words, in some cases Example: when deleting a bold word if the word is the only in the textarea.

--- projects/angular-editor/src/lib/angular-editor.component.ts
+++ projects/angular-editor/src/lib/angular-editor.component.ts
@@ -205,7 +205,7 @@ export class AngularEditorComponent implements OnInit, ControlValueAccessor, Aft
    * @param fn a function
    */
   registerOnChange(fn: any): void {
-    this.onChange = fn;
+    this.onChange = e => (e === '<br>' ? fn('') : fn(e)) ;
   }
 
   /**
