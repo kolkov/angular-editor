d9fa5bee1918366ae2bb681b6f22798b047f0abf
Alexandre <alex.paprocki@gmail.com>
2020-09-29 18:39:55 +0200

    Update fontSolidIcon V5

--- projects/angular-editor/src/lib/angular-editor.module.ts
+++ projects/angular-editor/src/lib/angular-editor.module.ts
@@ -4,13 +4,45 @@ import {AngularEditorToolbarComponent} from './angular-editor-toolbar.component'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { AeSelectComponent } from './ae-select/ae-select.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faItalic, faBold, faSuperscript, faSubscript, faUnderline, faUndo, faAlignCenter, faAlignLeft, faAlignJustify, faAlignRight, faRedo, faStrikethrough, faIndent, faOutdent, faListOl, faListUl, faFont, faLink, faUnlink, faImage, faVideo, faMinus, faCode, faRemoveFormat } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule
+    CommonModule, FormsModule, ReactiveFormsModule,FontAwesomeModule
   ],
   declarations: [AngularEditorComponent, AngularEditorToolbarComponent, AeSelectComponent],
   exports: [AngularEditorComponent, AngularEditorToolbarComponent]
 })
 export class AngularEditorModule {
+  constructor(library: FaIconLibrary) {
+
+    library.addIcons(
+      faItalic,
+      faBold,
+      faSuperscript,
+      faSubscript,
+      faUnderline,
+      faUndo,
+      faAlignRight,
+      faAlignCenter,
+      faAlignLeft,
+      faAlignJustify,
+      faRedo,
+      faStrikethrough,
+      faIndent,
+      faOutdent,
+      faListUl,
+      faListOl,
+      faFont,
+      faLink,
+      faUnlink,
+      faImage,
+      faVideo,
+      faMinus,
+      faRemoveFormat,
+      faCode
+
+    );
+  }
 }
