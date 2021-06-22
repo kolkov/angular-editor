6aa2fa4effcfb912acc9aaba9a479d400809d14a
Andy <a.kolkov@gmail.com>
2021-06-22 14:38:54 +0300

    ci(karma): Fix Karma config

--- projects/angular-editor/karma.conf.js
+++ projects/angular-editor/karma.conf.js
@@ -13,12 +13,21 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      type: "lcov",
       dir: require('path').join(__dirname, '../../coverage/angular-editor'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
