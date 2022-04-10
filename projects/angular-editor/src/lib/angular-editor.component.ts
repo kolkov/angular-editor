721fbfd15582d3374cd187c6d72ef4a87637f707
Nikita Petrov <mmmadaro@gmail.com>
2022-04-10 09:16:12 +0300

    Allow to alter styleWithCSS flag

--- projects/angular-editor/src/lib/angular-editor.component.ts
+++ projects/angular-editor/src/lib/angular-editor.component.ts
@@ -96,6 +96,9 @@ export class AngularEditorComponent implements OnInit, ControlValueAccessor, Aft
 
   ngOnInit() {
     this.config.toolbarPosition = this.config.toolbarPosition ? this.config.toolbarPosition : angularEditorConfig.toolbarPosition;
+    if (this.config.styleWithCSS) {
+        this.doc.execCommand('styleWithCSS', null, true);
+    }
   }
 
   ngAfterViewInit() {
@@ -108,7 +111,7 @@ export class AngularEditorComponent implements OnInit, ControlValueAccessor, Aft
     if (this.config.rawPaste) {
       event.preventDefault();
       const text = event.clipboardData.getData('text/plain');
-      document.execCommand('insertHTML', false, text);
+      this.doc.execCommand('insertHTML', false, text);
       return text;
     }
   }
